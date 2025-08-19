6d6dacaa7933d7350f291cc385d98f0088ced6e0
OldManJenkins <96033841+MilkCat03@users.noreply.github.com>
2025-08-19 17:31:45 +0000

    idk

--- ublock orgin/Execute Bookmarklets Using Backslash.js
+++
@@ -1,9 +0,0 @@
-/// executeScripts.js
-document.addEventListener('keydown', function(event) {
-  if (event.key === '\\' || event.keyCode === 220) {
-    const userInput = prompt("What script would you like to execute?");
-    const script = document.createElement('script');
-    script.textContent = userInput;
-    document.body.appendChild(script);
-  }
-});
